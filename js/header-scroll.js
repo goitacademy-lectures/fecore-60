13dca9ba98ff1462645fb95461b33d80d3f03453
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2022-07-22 00:37:19 +0300

    add module 05 practice

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -2,7 +2,7 @@ window.onscroll = () => changeHeaderBackground();
 
 function changeHeaderBackground() {
   const header = document.querySelector('[data-header]');
-  const headerOffsetTrigger = header.offsetTop;
+  const headerOffsetTrigger = header.offsetHeight;
   const pageOffset = window.pageYOffset;
 
   if (pageOffset > headerOffsetTrigger) {
